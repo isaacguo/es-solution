dfdc1beacba334ff2fc8f339f0ba0f139a7d5573
GUO,YUE (K-China,ex1) <yue_guo@keysight.com>
2017-11-22 15:54:52 +0800

    added

--- es-slack/src/main/java/com/keysight/es/services/SlackService.java
+++ es-slack/src/main/java/com/keysight/es/services/SlackService.java
@@ -1,4 +1,15 @@
 package com.keysight.es.services;
 
-public class SlackService {
+
+import com.keysight.es.dto.MessageBody;
+import com.keysight.es.entities.SlackBotInfoEntity;
+
+import java.util.List;
+
+public interface SlackService {
+    String postMessageToSlack(MessageBody messageBody);
+
+    List<SlackBotInfoEntity> getWebhooksInfo();
+
+    void createWebhookInfo(SlackBotInfoEntity slackBotInfoEntity);
 }
